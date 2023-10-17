e7d5f86c165b3466de047e5bc52b90900acfe574
ShreyashChaugule-github <cshreyash219@gmail.com>
2023-10-17 14:23:44 +0530

    React Based Simple Website

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>Techystar</h1>
+            <p>@all right Reserved</p>
+        </div>
+
+        <div>
+            <h5>Follow Us</h5>
+                <div>
+                    <a href="https://www.youtube.com/" traget={"blank"}> Youtube</a>
+                    <a href="https://www.Instagram.com/" traget={"blank"}> Instagram</a>
+                    <a href="https://www.Facebook.com/" traget={"blank"}> facebook</a>
+                </div>
+            </div>
+    </footer>
+  )
+}
+
+export default Footer
